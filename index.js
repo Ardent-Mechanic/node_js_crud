5adb575b3b65604e0ffdf81faef962f6103b94f6
Ustyantsev_Daniil <gikata.38@yandex.ru>
2023-03-18 01:45:32 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const http = require('http');
 const PORT = process.env.PORT || 5000;
+const Application = require('./module/Application');
+const userRouter = require('./data_base/film_router');
+const jsonParser = require('./module/parseJson');
+const parseUrl = require('./module/parseUrl');
 
 
-const server = http.createServer((request, response) => {
-    console.log("Url: " + request.url);
-    console.log("Тип запроса: " + request.method);
-    // console.log("User-Agent: " + request.headers["user-agent"]);
-    // console.log("Все заголовки");
-    // console.log(request.headers);
-    response.end();
-});
+const app = new Application()
+
+app.use(jsonParser);
+app.use(parseUrl('http://localhost:5000'));
+
+app.addRouter(userRouter);
+
 
 const start = async () => {
     try {
-        // await mongoose.connect('mongodb+srv://user:123@cluster0.4comu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-
-
-        server.listen(PORT, () => {
-            console.log(`Server running at hhtp://localhost:${PORT}/`)
-        });
+        app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
 }
 
