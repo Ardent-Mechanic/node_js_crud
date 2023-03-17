310824766774ea9483dd7dcc2ae2e79e17f4f572
Ustyantsev_Daniil <gikata.38@yandex.ru>
2023-03-18 02:48:18 +0300

    fix update func

--- data_base/film_controller.js
+++ data_base/film_controller.js
@@ -6,6 +6,7 @@ const getFilm =  async (req, res) => {
     try {
         const newFilm = await Film.query(
             "SELECT * FROM films");
+
         res.send(newFilm.rows);
     } catch (err) {
         console.log(err);
@@ -16,7 +17,9 @@ const createFilm = async (req, res) => {
     try {
         const {filmName, filmDate} = req.body;
         const newFilm = await Film.query(
-            "INSERT INTO films (film_name, year_of_production) values ($1, $2) RETURNING *", [filmName, filmDate])
+            "INSERT INTO films (film_name, year_of_production) values ($1, $2) RETURNING *",
+            [filmName, filmDate])
+
         res.send(newFilm.rows[0]);
     } catch (err) {
         console.log(err);
@@ -29,9 +32,11 @@ const deleteFilm = async (req, res) => {
 
 const updateFilm = async (req, res) => {
     try{
-        const {filmName, filmDate} = req.body;
+        const {filmId, filmName, filmDate} = req.body;
         const newFilm = await Film.query(
-            "UPDATE films set film_name = $1, set year_of_production = $2 RETURNING *", [filmName, filmDate]);
+            "UPDATE films SET (film_name, year_of_production) = ($1, $2) WHERE film_id = $3 RETURNING *;",
+            [filmName, filmDate, filmId]);
+
         res.send(newFilm.rows[0]);
     } catch (err) {
     console.log(err);
