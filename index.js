992c84c886f0a752026293383a08119e4793c664
Ustyantsev_Daniil <gikata.38@yandex.ru>
2023-03-18 03:36:21 +0300

    add genre router in index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 const Application = require('./module/Application');
-const userRouter = require('./data_base/film_router');
+const filmRouter = require('./data_base/film_router');
+const genreRouter = require('./data_base/genre_router');
 const jsonParser = require('./module/parseJson');
 const parseUrl = require('./module/parseUrl');
 
@@ -11,7 +12,8 @@ const app = new Application()
 app.use(jsonParser);
 app.use(parseUrl('http://localhost:5000'));
 
-app.addRouter(userRouter);
+app.addRouter(filmRouter);
+app.addRouter(genreRouter);
 
 
 const start = async () => {
