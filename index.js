f212349b69dbf863906a331322c8fd05fe08f65d
Ustyantsev_Daniil <gikata.38@yandex.ru>
2023-03-18 04:54:18 +0300

    final version

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 const Application = require('./module/Application');
-const filmRouter = require('./data_base/film_router');
-const genreRouter = require('./data_base/genre_router');
+const filmRouter = require('./routers/film_router');
+const genreRouter = require('./routers/genre_router');
+const filmGenreRouter = require('./routers/film_genre_router');
 const jsonParser = require('./module/parseJson');
 const parseUrl = require('./module/parseUrl');
 
@@ -14,6 +15,7 @@ app.use(parseUrl('http://localhost:5000'));
 
 app.addRouter(filmRouter);
 app.addRouter(genreRouter);
+app.addRouter(filmGenreRouter);
 
 
 const start = async () => {
